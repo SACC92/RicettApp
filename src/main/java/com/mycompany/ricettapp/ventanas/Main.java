d9cf9480abd74ed4beea29a3f38b419f6faf73d7
Jorge Echeverria <1jorge.echeverria@gmail.com>
2018-06-18 22:29:01 -0400

    Proyecto en Maven

---
+++ src/main/java/com/mycompany/ricettapp/ventanas/Main.java
@@ -0,0 +1,10 @@
+package ventanas;
+
+public class Main {
+    
+    public static void main (String[]args){
+        GuiMain ventana = new GuiMain("Menu"); 
+
+        ventana.setVisible(true);
+    }    
+}
