b999a656f9155f35597f4ce84d5de7bc79c7cd0c
Jorge Echeverria <1jorge.echeverria@gmail.com>
2018-06-19 21:08:23 -0400

    Rial Ordenado

---
+++ src/main/java/com/mycompany/ricettapp/ventanas/Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.ricettapp.ventanas;
+
+public class Main {
+    
+    public static void main (String[]args){
+        
+        GuiMain ventana = new GuiMain("Menu"); 
+        ventana.setVisible(true);
+        
+    }    
+}
