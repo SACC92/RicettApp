9f4db1c01dc2d8a70a49b93cf7e40d73d4d79206
Jorge Echeverria <1jorge.echeverria@gmail.com>
2018-06-20 18:30:21 -0400

    Fix Prop GuiBorrar

--- src/main/java/com/mycompany/ricettapp/ventanas/GuiBorrarReceta.java
+++ src/main/java/com/mycompany/ricettapp/ventanas/GuiBorrarReceta.java
@@ -81,10 +81,10 @@ public class GuiBorrarReceta extends JFrame implements ActionListener{
                 this.add(jPanel2);
                 this.add(jPanel3);
                 
-		this.setLocationRelativeTo(null);
-		this.setSize(500, 250);
-		this.setMinimumSize(this.getSize());
-		this.setMaximumSize(this.getSize());
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                setSize(700, 200);
+                setLocationRelativeTo(null);
+                setResizable(false);
                 
                 this.setVisible(true);
     }
