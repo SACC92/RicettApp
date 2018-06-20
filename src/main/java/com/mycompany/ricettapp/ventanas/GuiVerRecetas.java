43dfd70bdcdc72b740e71d83d5900d72863faee0
Lucas Sandoval <l.sandoval09@ufromail.cl>
2018-06-19 23:20:28 -0400

    area

--- src/main/java/com/mycompany/ricettapp/ventanas/GuiVerRecetas.java
+++ src/main/java/com/mycompany/ricettapp/ventanas/GuiVerRecetas.java
@@ -1,30 +1,131 @@
 package com.mycompany.ricettapp.ventanas;
 
-import java.awt.Container;
+import com.mycompany.ricettapp.funciones.Recetario;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.HeadlessException;
-import javax.swing.JFrame;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultListModel;
 
-public class GuiVerRecetas extends JFrame {	
-	
-	private PanelJlist panelList;
-	
-	public GuiVerRecetas(String title) throws HeadlessException {
-		
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
+
+public class GuiVerRecetas extends JFrame implements ActionListener{
+    
+	private JScrollPane menuScrollPane;
+        private JScrollPane areaScrollPane;
+        
+        protected JList listaRecetas;
+        
+        protected JLabel jLabel1;
+        protected JLabel jLabel2;
+        protected JPanel jPanel1;
+
+        protected JPanel jPanel2;
+        protected JButton bVer;
+
+        protected JPanel jPanel3;
+        protected JTextArea area;
+
+
+	public GuiVerRecetas(String title) {
                 super(title);
-		this.panelList = new PanelJlist();
-		this.setContentPane(panelList);
-		
-		
+                
+		FlowLayout layout = new FlowLayout();                
+		this.setLayout(layout);
+                
+                jPanel1 = new JPanel();
+                bVer = new JButton("Ver receta");
+                jPanel2 = new JPanel();
+                jLabel1 = new JLabel("Recetas:");
+                
+                 //para el JList y barra
+                    Recetario recetario = new Recetario();
+                     DefaultListModel listModel = new DefaultListModel();
+                        for (int x = 0; x < recetario.recetas.size(); x++) {
+                            listModel.addElement(recetario.recetas.get(x).getNombre());
+                                 }
+                            listaRecetas = new JList();
+                            listaRecetas.setModel(listModel);
+                            
+		menuScrollPane = new JScrollPane(listaRecetas);
+                menuScrollPane.setPreferredSize(new Dimension(100,100));
+                 //
+                
+                jPanel3 = new JPanel();
+                listaRecetas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+                jPanel3 = new JPanel();
+                area = new JTextArea();                
+                areaScrollPane = new JScrollPane(area);
+                areaScrollPane.setPreferredSize(new Dimension(100,100));
+
+                //Para saber la cantidad de recetas almacenadas        
+                int cantidad = recetario.verCantidadRecetas();
+                jLabel2 = new JLabel(String.valueOf(cantidad));
+
+                //agregar los comportamientos a los obejtos de loa ventana
+                bVer.addActionListener(this);
+
+                // agregar objetos a la ventana
+                // uso de paneles para ordenar la distribucion de objetos en la ventana
+                jPanel1.add(jLabel1);
+                jPanel1.add(jLabel2);
+
+                jPanel2.add(bVer);
+
+                jPanel3.add(areaScrollPane);
+
+                this.add(jPanel1);
+                //para la barra
+                this.add(menuScrollPane);
+                this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+                //
+                this.add(jPanel2);
+                this.add(jPanel3);
+                
 		this.setLocationRelativeTo(null);
-		this.setSize(550, 550);
-                setResizable(false);
+		this.setSize(500, 250);
 		this.setMinimumSize(this.getSize());
 		this.setMaximumSize(this.getSize());
-                setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setVisible(true);
-
+                
+                this.setVisible(true);
 	}
+        
+        @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if (e.getSource() == bVer) {          
+            int i = listaRecetas.getSelectedIndex();
+            mostrar(i); 
+        }
+    }
+	
+	void mostrar(int i){         
+        if(i>-1){
+        Recetario recetario = new Recetario();
+        String ingredientes = recetario.obtenerIngredientes(i); 
+        String instrucciones = recetario.obtenerInstrucciones(i); 
+        
+        recetario.recetas.get(i).rankear();
+        
+        area.setText("Los datos de la receta son:"+ "\nNombre:"+" "+recetario.recetas.get(i).getNombre() 
+                + "\nRanking:"+ recetario.recetas.get(i).getRanking()
+                + "\nIngredientes:"+ ingredientes 
+                + "\nInstrucciones:"+instrucciones);
+         }
+        else{
+             area.setText("Seleccione una receta de la lista"+ 
+                          "\npara poder realizar esta acción");
+            } 
+    }
 
 }
