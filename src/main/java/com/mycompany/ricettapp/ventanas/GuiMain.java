d60930767b499288ba0512e9cc31b034efc39c55
Jorge Echeverria <1jorge.echeverria@gmail.com>
2018-06-25 21:27:14 -0400

    Ordenamiento GuiMain

--- src/main/java/com/mycompany/ricettapp/ventanas/GuiMain.java
+++ src/main/java/com/mycompany/ricettapp/ventanas/GuiMain.java
@@ -1,18 +1,17 @@
 package com.mycompany.ricettapp.ventanas;
 
 import java.awt.FlowLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class GuiMain extends JFrame implements ActionListener {
 
-    protected JButton bVerRecetas;
-    protected JButton bAgregarReceta;
-    protected JButton bBorrarReceta;
-    protected JButton bIngredientes;
-    protected JPanel jPanel;
+    protected JButton verRecetasB;
+    protected JButton agregarRecetaB;
+    protected JButton borrarRecetaB;
+    protected JButton ingredientesB;
+    protected JPanel botoneraP;
 
     GuiMain(String title) {
 
@@ -20,28 +19,27 @@ public class GuiMain extends JFrame implements ActionListener {
         this.setLayout(new FlowLayout());
 
         //instanciar los objetos de la ventana
-        bVerRecetas = new JButton("Ver Recetas");
-        bAgregarReceta = new JButton("Agregar Receta");
-        bBorrarReceta = new JButton("Borrar Receta");
-        bIngredientes = new JButton("Buscar Ingredientes");
-        jPanel = new JPanel();
+        verRecetasB = new JButton("Ver Recetas");
+        agregarRecetaB = new JButton("Agregar Receta");
+        borrarRecetaB = new JButton("Borrar Receta");
+        ingredientesB = new JButton("Buscar Ingredientes");
+        botoneraP = new JPanel();
 
         //agregar los comportamientos a los obejtos de loa ventana
-        bVerRecetas.addActionListener(this);
-        bAgregarReceta.addActionListener(this);
-        bBorrarReceta.addActionListener(this);
-        bIngredientes.addActionListener(this);
+        verRecetasB.addActionListener(this);
+        agregarRecetaB.addActionListener(this);
+        borrarRecetaB.addActionListener(this);
+        ingredientesB.addActionListener(this);
 
         // agregar objetos a la ventana
-        // uso de paneles para ordenar la distribucion de objetos en la ventana
-        jPanel.add(bVerRecetas);
-        jPanel.add(bAgregarReceta);
-        jPanel.add(bBorrarReceta);
-        jPanel.add(bIngredientes);
+        botoneraP.add(verRecetasB);
+        botoneraP.add(agregarRecetaB);
+        botoneraP.add(borrarRecetaB);
+        botoneraP.add(ingredientesB);
 
-        this.add(jPanel);
+        this.add(botoneraP);
 
-        //Operaciones por defecto
+        //Configuracion de la ventana
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 200);
         setLocationRelativeTo(null);
@@ -51,19 +49,19 @@ public class GuiMain extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        if (e.getSource() == bVerRecetas) {
+        if (e.getSource() == verRecetasB) {
             GuiVerRecetas ventRecet = new GuiVerRecetas("Ver Recetas");
             ventRecet.setVisible(true);
         }
-        if (e.getSource() == bAgregarReceta) {
+        if (e.getSource() == agregarRecetaB) {
             GuiAdd ventana = new GuiAdd("Agregar Receta");
             ventana.setVisible(true);
         }
-        if (e.getSource() == bBorrarReceta) {
+        if (e.getSource() == borrarRecetaB) {
             GuiBorrarReceta ventB = new GuiBorrarReceta("Borrar Receta");
             ventB.setVisible(true);
         }
-        if (e.getSource() == bIngredientes) {
+        if (e.getSource() == ingredientesB) {
             GuiBuscarIng buscar = new GuiBuscarIng("Buscar Ingrediente");
             buscar.setVisible(true);
         }
