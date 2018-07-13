7c8db7403be2da982424b5e87a36df9f1321e66e
Jorge Echeverria <1jorge.echeverria@gmail.com>
2018-07-12 21:34:15 -0400

    GuiAdd Receta Existe?

--- src/main/java/com/mycompany/ricettapp/ventanas/GuiMain.java
+++ src/main/java/com/mycompany/ricettapp/ventanas/GuiMain.java
@@ -1,7 +1,6 @@
 package com.mycompany.ricettapp.ventanas;
 
 import java.awt.FlowLayout;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
